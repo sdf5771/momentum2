2e0ec47418791ad9e3f61bef46d225fee84e923b
SeobisBack <seobisback@gmail.com>
2021-12-23 10:17:09 +0900

    make background randomized

--- js/background.js
+++ js/background.js
@@ -1,5 +1,13 @@
-const imaages = [
+const images = [
     "0.jpeg",
     "1.jpeg",
     "2.jpeg",
-]
+];
+//image randomize
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement('img');
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
