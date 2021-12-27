0f2906cf5cf655e8fa78a7f7b3f2109e01f0ce5c
SeobisBack <seobisback@gmail.com>
2021-12-27 17:08:29 +0900

    CSS

--- js/background.js
+++ js/background.js
@@ -8,6 +8,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement('img');
 
+bgImage.className = 'bgImg';
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
