615d9e19b84df289b2ba56ddc2f22e61add28907
SeobisBack <seobisback@gmail.com>
2021-12-27 16:21:24 +0900

    done connect weather api

--- js/weather.js
+++ js/weather.js
@@ -4,8 +4,16 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log(`You live in ${lat} , ${lon}`);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector('#weather span:first-child');
+            const city = document.querySelector('#weather span:last-child');
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGoeError() {
     alert("Can't find your location, There is No Weather for you.");
