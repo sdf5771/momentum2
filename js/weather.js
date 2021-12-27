7e7ab9e4affc106a0f1d5731acd91254f8ab505b
SeobisBack <seobisback@gmail.com>
2021-12-27 17:15:27 +0900

    change little bit

--- js/weather.js
+++ js/weather.js
@@ -12,7 +12,7 @@ function onGeoOk(position) {
             const weather = document.querySelector('#weather span:first-child');
             const city = document.querySelector('#weather span:last-child');
             city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+            weather.innerText = `🌏${data.weather[0].main} / ${data.main.temp}°C`;
     });
 }
 function onGoeError() {
